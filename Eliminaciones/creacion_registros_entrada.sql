1dad1bb023d0558edcb56fbf5278979d5afd22aa
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-11-07 11:56:12 -0500

    Reto 6: Eliminaciones

---
+++ Eliminaciones/creacion_registros_entrada.sql
@@ -0,0 +1,51 @@
+create table registros_entrada(
+	codigo_registro int not null,
+	cedula_empleado char(10) not null,
+	fecha date not null,
+	hora time not null,
+	constraint codigo_registro_pk primary key(codigo_registro)
+)
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(12345,'2121234929','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(92838,'9021218821','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(09384,'9182949222','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(10384,'9284929482','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(20495,'8482749384','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(29847,'5737272834','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(54534,'2182832821','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(09485,'2184928129','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(92182,'2194839123','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(43932,'4737173843','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(21021,'4627374628','14/05/2025','13:56')
+
+insert into registros_entrada(codigo_registro,cedula_empleado,fecha,hora)
+values(24343,'4627374628','14/08/2025','13:56')
+
+select * from registros_entrada
+
+update registros_entrada set cedula_empleado = '082345679'
+where fecha between '01/08/2025' and '31/08/2025'
+
+delete from registros_entrada
+where fecha between '01/05/2025' and '30/05/2025'
---
+++ Eliminaciones/personas.sql
@@ -0,0 +1,44 @@
+create table personas(
+	cedula char(10) not null,
+	nombre varchar(50) not null,
+	apellido varchar(50) not null,
+	estatura decimal,
+	fecha_nacimiento date,
+	hora_nacimiento time,
+	cantidad_ahorrada money,
+	numero_hijos int,
+	constraint cedula_pk primary key(cedula)
+)
+insert into personas(cedula,nombre,apellido)
+values ('1727392035','Brayan','Narvaez')
+
+insert into personas(cedula,nombre,apellido, estatura)
+values ('1234567890','Nelson','Angulo',1.85)
+
+insert into personas(cedula,nombre,apellido,numero_hijos)
+values ('1212121212','Diego','Maradona',3)
+
+insert into personas(cedula,nombre,apellido,estatura,fecha_nacimiento,hora_nacimiento,cantidad_ahorrada,numero_hijos)
+values ('1334243243','Pepe','Menedoza',1.60,'04/10/2025','12:35',500.54,3)
+
+select cedula,nombre,numero_hijos,estatura from personas
+
+select * from personas
+
+update personas set estatura = 1.70
+where cedula = '1727392035'
+
+update personas set cantidad_ahorrada = 0.0
+where cantidad_ahorrada is null
+
+update personas set numero_hijos = 0
+where numero_hijos is null
+
+update personas set estatura = 1.80, fecha_nacimiento = '10/10/2025'
+where cedula = '1727392035'
+
+delete from personas 
+where cedula = '1234567890'
+
+delete from personas
+where estatura is null
