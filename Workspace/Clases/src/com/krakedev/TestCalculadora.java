0e45ee504a61ec950498fc758320436306559915
BrayanNarvaez-25 <brayann251107g@gmail.com>
2025-11-21 17:48:38 -0500

    Reto 9: Métodos

---
+++ Workspace/Clases/src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,41 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora calcu = new Calculadora();
+		int resultadoSuma;
+		double resultadoResta;
+		double resultadoMultiplicar;
+		double resultadoDividir;
+		double resultadoProm;
+		
+		resultadoSuma = calcu.sumar(5,3);
+		System.out.println("Resultado Suma: " + resultadoSuma);
+	
+		System.out.println("***************");
+		
+		resultadoResta = calcu.restar(10, 3);
+		System.out.println("Resultado Resta: " + resultadoResta);
+		
+		System.out.println("***************");
+	
+		resultadoMultiplicar = calcu.multiplicar(3, 4);
+		System.out.println("Resultado Multiplicación: "+ resultadoMultiplicar);
+		
+		System.out.println("***************");
+	
+		resultadoDividir = calcu.dividir(6, 4);
+		System.out.println("Resultado División: "+ resultadoDividir);
+		
+		System.out.println("***************");
+		
+		resultadoProm = calcu.promediar(4, 6, 8);
+		System.out.println("Resultado Promedio: "+ resultadoProm);
+		
+		System.out.println("***************");
+		
+		calcu.mostrarResultado();
+	}
+
+}
